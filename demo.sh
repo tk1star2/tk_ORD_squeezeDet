ddc944da8ac29c86d05128ef9303520e8f2efa78
TAEKOAN <tk1star2@nate.com>
2018-05-04 09:32:50 +0000

    revised1

---
+++ demo.sh
@@ -0,0 +1,1 @@
+GOMP_CPU_AFFINITY="7" OMP_NUM_THREADS=1 python ./src/demo_hayun.py
---
+++ demo2.sh
@@ -0,0 +1,1 @@
+python ./src/demo_hayun.py --demo_net=squeezeDet+ --checkpoint=./data/model_checkpoints/squeezeDetPlus/model.ckpt-95000
